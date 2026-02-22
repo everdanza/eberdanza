1995e5c2e013206c3e910b6f51daefbb7de6bd1b
everdanza <everdanzaweb@gmail.com>
2026-02-22 05:02:39 -0300

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZU_RioqPL9BBByP58r5VaXNa8tJAwhxc",
+  authDomain: "everdanza.firebaseapp.com",
+  projectId: "everdanza",
+  storageBucket: "everdanza.firebasestorage.app",
+  messagingSenderId: "509362704874",
+  appId: "1:509362704874:web:e21bb25a341ac7e6c2e293",
+  measurementId: "G-PTDSS0TXJV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
