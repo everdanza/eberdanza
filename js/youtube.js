be40817b84df3c25302159bc4a01bfd43390e3e4
everdanza <everdanzaweb@gmail.com>
2026-02-22 12:02:21 -0300

    ewrw

--- js/youtube.js
+++ js/youtube.js
@@ -1,7 +1,9 @@
 const VIDEO_DATA = {
-  url: "https://www.youtube.com/watch?v=cHfrfyDLfgw",
-  title: "Highlight del stream – Everdanza",
-  description: "Un clip destacado del último stream de la comunidad Everdanza."
+  url: 'https://www.youtube.com/watch?v=cHfrfyDLfgw',
+  title: '¿POR QUE NO VEMOS CINE ARGENTINO? - Suficiente Internet',
+  description: '🎬 ¿Por qué hay gente que dice “el cine argentino es malo” aunque no vio ni tres películas?\nEn este video me meto con el mito más grande de la industria: el odio injustificado al cine argento. ¿Es todo drama? ¿Todo en un departamento? ¿Todo financiado por “Ricardo INCAA”? Spoiler: no.';
+  fecha: '15/02/2026'
+
 };
 
 function getVideoId(url) {
@@ -27,6 +29,7 @@ function renderYouTubeVideo() {
 
     <div class="youtube-title">${VIDEO_DATA.title}</div>
     <div class="youtube-description">${VIDEO_DATA.description}</div>
+    <div class="youtube-fecha">${VIDEO_DATA.description}</div>
   `;
 }
 
